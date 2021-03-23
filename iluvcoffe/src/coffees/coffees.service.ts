bcada1596286bac743d68d89ad2d6866eaeafaf7
agungTuanany <agung.tuanany@gmail.com>
2021-03-23 15:01:21 +0700

    feature(master): chapter-2: creating rest-api | send user-friendly error message

--- iluvcoffe/src/coffees/coffees.service.ts
+++ iluvcoffe/src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +17,16 @@ export class CoffeesService {
     }
 
     findOne(id: string) {
-        return this.coffees.find(item => item.id === +id);
+        throw "A Random Error";
+        const coffee = this.coffees.find(item => item.id === +id);
+
+        if (!coffee) {
+            // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+            throw new NotFoundException(`Coffee #${id} not found`);
+        }
+
+        return coffee;
+        // return this.coffees.find(item => item.id === +id);
     }
 
     create(createCoffeDto: any) {
