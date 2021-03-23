db37fc3521d2943296f1d4697a7c552028022fc9
agungTuanany <agung.tuanany@gmail.com>
2021-03-23 12:48:25 +0700

    feature(master): chapter-2: creating rest-api | create a basic service

--- iluvcoffe/src/coffees/coffees.controller.ts
+++ iluvcoffe/src/coffees/coffees.controller.ts
@@ -1,59 +1,40 @@
 import { Controller, Get, Param, Body, Post, HttpCode, HttpStatus, Res, Patch, Delete, Query } from '@nestjs/common';
+import { CoffeesService }from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
 
+    constructor(private readonly coffeesService:  CoffeesService) {}
+
     @Get()
     findAll(@Query() paginationQuery) {
         const { limit, offset } = paginationQuery;
-        return `This action returns all the coffees. Limit ${limit}, offset: ${offset}`;
+        return this.coffeesService.findAll();
+        // return `This action returns all the coffees. Limit ${limit}, offset: ${offset}`;
     }
 
-    // @Get()
-    // findAll() {
-    //     return "This action returns all the coffees.";
-    // }
-
-
-    // Or with native handle status use '@Res()'
-    // XXX CAUTION: it's not the best practice to use native handle status code. XXX
-    // @Get()
-    // findAll(@Res() response) {
-    //     response.status(200).send("This action returns all the coffees.");
-    //     // return "This action returns all the coffees.";
-    // }
-
-    // @Get(':id')
-    // findOne(@Param() params) {
-    //     return `This action returns ==> #${params.id} the coffees.`;
-    // }
-
-    // Or with constraint Param object
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `This action returns #${id} the coffees.`;
+        return this.coffeesService.findOne(id);
+        // return `This action returns #${id} the coffees.`;
     }
 
     @Post()
     @HttpCode(HttpStatus.GONE)
     create(@Body() body) {
-        return body;
+        return this.coffeesService.create(body);
+        // return body;
     }
-
-    // Or with constrain Body object
-    // XXX CAUTION: other properties WON'T be validated XXX
-    // @Post()
-    // create(@Body('name') body) {
-    //     return body;
-    // }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body) {
-        return `This action returns #${id} the coffees.`;
+        return this.coffeesService.update(id, body);
+        // return `This action returns #${id} the coffees.`;
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return `This action removes #${id} coffee`;
+        return this.coffeesService.remove(id);
+        // return `This action removes #${id} coffee`;
     }
 }
---
+++ iluvcoffe/src/coffees/coffees.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+    private coffees: Coffee[] = [
+        {
+            id: 1,
+            name: 'Salemba Roast',
+            brand: 'Salemba Brand',
+            flavors: ['chocolate', 'vanilla'],
+        },
+    ];
+
+    findAll() {
+        return this.coffees;
+    }
+
+    findOne(id: string) {
+        return this.coffees.find(item => item.id === +id);
+    }
+
+    create(createCoffeDto: any) {
+        this.coffees.push(createCoffeDto);
+    }
+
+    update(id: string, updateCoffeDto: any) {
+        const existingCoffee = this.findOne(id);
+
+        if (existingCoffee) {
+            // update the existing entity
+        }
+    }
+
+    remove(id: string) {
+        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+
+        if (coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1);
+        }
+    }
+}
