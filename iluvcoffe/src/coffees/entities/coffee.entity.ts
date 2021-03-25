2861ec423739d197e8de748c456664d3344f6b8c
agungTuanany <agung.tuanany@gmail.com>
2021-03-25 18:59:30 +0700

    feature(master): chapter-3: add postgresql with typeorm | creating a typeorm entity

--- iluvcoffe/src/coffees/entities/coffee.entity.ts
+++ iluvcoffe/src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()   // sql table === "coffee"
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true})
     flavors: string[];
 }
