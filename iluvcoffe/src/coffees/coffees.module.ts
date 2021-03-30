81809d487059691b9199349863b17d9a41299501
agungTuanany <agung.tuanany@gmail.com>
2021-03-30 18:57:21 +0700

    feature(master): chapter-4:  dependency injection | Class based Provider

--- iluvcoffe/src/coffees/coffees.module.ts
+++ iluvcoffe/src/coffees/coffees.module.ts
@@ -8,13 +8,26 @@ import { Flavor } from "./entities/flavor.entity";
 import { Event } from "../events/entities/event.entity";
 import { COFFEE_BRANDS } from "./coffees.constants";
 
-class MockCoffeesService {}
+class ConfigService {} 			// <<<
+class DevelopmentConfigService {} 	// <<<
+class ProductionConfigService {} 	// <<<
+// class MockCoffeesService {}
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     // providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
-    providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ["Salemba brew", "nestcafe"] }],
+    // providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ["Salemba brew", "nestcafe"] }],
+    providers: [
+        CoffeesService,
+        {
+            provide: ConfigService,
+            useClass: process.env.NODE_ENV === "development" ? DevelopmentConfigService : ProductionConfigService,
+        },
+        {
+            provide: COFFEE_BRANDS, useValue: ["Salemba brew", "nestcasfe"]
+        }
+    ],
     exports: [CoffeesService],
 })
 export class CoffeesModule {}
