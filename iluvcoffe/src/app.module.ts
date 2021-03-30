69f178ba3134d7532d31a978a1c5881fb3830cd7
agungTuanany <agung.tuanany@gmail.com>
2021-03-31 05:55:09 +0700

    feature(master): chapter-4:  dependency injection | create a dynamic module

--- iluvcoffe/src/app.module.ts
+++ iluvcoffe/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module";
+import { DatabaseModule } from './database/database.module';
 
 @Module({
     imports: [
@@ -21,6 +22,7 @@ import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module";
             synchronize: true, // COMMENT THIS in production - mode
         }),
         CoffeeRatingModule,
+        DatabaseModule,
     ],
     controllers: [AppController],
     providers: [AppService],
--- iluvcoffe/src/coffee-rating/coffee-rating.module.ts
+++ iluvcoffe/src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { CoffeesModule } from "../coffees/coffees.module";
 import { CoffeeRatingService } from "./coffee-rating.service";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
-    imports: [CoffeesModule],
+    imports: [
+        DatabaseModule.register({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "pass123",
+            // database: "postgres1",
+            // autoLoadEntities: true,
+            // synchronize: true, // COMMENT THIS in production - mode
+        }),
+        CoffeesModule
+    ],
     providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
