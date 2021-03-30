eb4eb8eb9444832a1530b5ca232a35c4312169ca
agungTuanany <agung.tuanany@gmail.com>
2021-03-30 15:19:37 +0700

    feature(master): chapter-4:  dependency injection | value based providers

--- iluvcoffe/src/coffees/coffees.module.ts
+++ iluvcoffe/src/coffees/coffees.module.ts
@@ -7,10 +7,12 @@ import { Coffee } from "./entities/coffee.entity";
 import { Flavor } from "./entities/flavor.entity";
 import { Event } from "../events/entities/event.entity";
 
+class MockCoffeesService {}
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [CoffeesService],
+    providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
     exports: [CoffeesService],
 })
 export class CoffeesModule {}
