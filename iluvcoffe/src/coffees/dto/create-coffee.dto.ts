0648af6e16f9b59bcd542916bdb2574cc78f8472
agungTuanany <agung.tuanany@gmail.com>
2021-04-08 23:45:54 +0700

    feature(master): chapter-7: generating openapi | decorating model properties

--- iluvcoffe/src/coffees/dto/create-coffee.dto.ts
+++ iluvcoffe/src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
 import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateCoffeeDto {
+    @ApiProperty({ description: "The name of a coffee." })
     @IsString()
     readonly name: string;
 
+    @ApiProperty({ description: "The brand of a coffee." })
     @IsString()
     readonly brand: string;
 
+    @ApiProperty({ example: [] })
     @IsString({ each: true })
     readonly flavors: string[];
 }
